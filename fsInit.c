dfa9bf82e3d494a14936933142e2fe38d7f7dc32
Student <diegofloresflores2001@gmail.com>
2023-04-04 01:08:11 -0700

    Progress done, need to init BitMap and root dir

--- fsInit.c
+++ fsInit.c
@@ -25,13 +25,33 @@
 #include "fs_structs.h"
 
 //Init: VCB, BitMap, DirEntry - Root
+// returns: 1 = sucess
+//		   -1 = error
 int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 {
-	VCB *vcb = malloc(sizeof(VCB));
-
-	printf("%ld\n", sizeof(VCB));
 	printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
+	VCB *vcb = malloc(blockSize);
+
+	LBAread(vcb, 1, 0);
+	if (vcb->magic_n == MAGIC_NUMBER)
+	{
+		free(vcb);
+		return 1;
+	}
+	vcb->magic_n = MAGIC_NUMBER;
+	vcb->block_size = blockSize;
+	vcb->number_of_blocks = numberOfBlocks;
+	vcb->free_blocks = numberOfBlocks;
+	vcb->bitmap_start = 1;
+	vcb->bitmap_end = (numberOfBlocks + (blockSize - 1)) / blockSize;
+	// root
+
+	
+	printf("###%d\n", vcb->bitmap_end);
+
+	BitMap *bitmap = malloc(sizeof(BitMap));
+	DirectoryEntry *dir_entry = malloc(sizeof(DirectoryEntry));
 
 	return 0;
 }
--- fs_structs.h
+++ fs_structs.h
@@ -1,24 +1,36 @@
 
-#ifndef FS_STRUCTS.H
+#ifndef FS_STRUCTS
 #define FS_STRUCTS .H
 #include <stdio.h>
 #include <time.h>
-#define MAX_BLOCKS 1000
-
+#define MAX_BLOCKS 1000 // 1000 * 512
+#define MAGIC_NUMBER 0x4261686100000000
 // Needs to take only 1 block
 typedef struct VCB
 {
-    unsigned int magic_n;          // Validity of File System
+    unsigned long magic_n;         // Validity of File System
     unsigned int root;             // Pos of root
     unsigned int block_size;       // Will be 512
     unsigned int free_blocks;      // Number of free blocks
     unsigned int number_of_blocks; // Total number of blocks in VCB
+    unsigned int bitmap_start;     // Starting pos of bitmap within VCB
+    unsigned int bitmap_end;       // Ending pos of bitmap within VCB
 } VCB;
 
+// 1000
+// VCB = start, end
+// Root = 20
+// 0 = VCB
+// 1 = BitMap
+// 2... 22
+// 3 = Home -> 23 and 43
+// 4 = pics -> 44 and 64 ....
+// Bitmap = 0  o 64, = 1
 // Could take multiple blocks
 typedef struct BitMap
 {
-    unsigned int bitmap[MAX_BLOCKS]; // To keep track of free spaces
+    unsigned int blocks;
+    unsigned int bitmap[]; // To keep track of free spaces
 } BitMap;
 
 typedef struct DirectoryEntry
