4fa35f59b63230e98b71698ede2649bae09b506f
DiegoF001 <diegofloresflores2001@gmail.com>
2023-04-04 18:31:38 -0700

    Bitmap has been initialized, and code has been refractored.

--- fsInit.c
+++ fsInit.c
@@ -24,6 +24,9 @@
 #include "mfs.h"
 #include "fs_structs.h"
 
+VCB * vcb;
+BitMap * bitmap;
+DirectoryEntry * dir_entry;
 //Init: VCB, BitMap, DirEntry - Root
 // returns: 1 = sucess
 //		   -1 = error
@@ -31,7 +34,7 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 {
 	printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
-	VCB *vcb = malloc(blockSize);
+	vcb = malloc(blockSize);
 
 	LBAread(vcb, 1, 0);
 	if (vcb->magic_n == MAGIC_NUMBER)
@@ -39,22 +42,52 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 		free(vcb);
 		return 1;
 	}
+	init_vcb(blockSize, numberOfBlocks);
+	init_bitmap();
+	test_bitmap();
+
+	// DirEntry
+	return 0;
+}
+
+int init_vcb(int blockSize, int numberOfBlocks){
 	vcb->magic_n = MAGIC_NUMBER;
 	vcb->block_size = blockSize;
 	vcb->number_of_blocks = numberOfBlocks;
 	vcb->free_blocks = numberOfBlocks;
-	vcb->bitmap_start = 1;
-	vcb->bitmap_end = (numberOfBlocks + (blockSize - 1)) / blockSize;
-	// root
-
-	
-	printf("###%d\n", vcb->bitmap_end);
-
-	BitMap *bitmap = malloc(sizeof(BitMap));
-	DirectoryEntry *dir_entry = malloc(sizeof(DirectoryEntry));
-
-	return 0;
+	int bitmap_blocks = 0;
+	// to bytes:
+	bitmap_blocks = (numberOfBlocks + (8 - 1)) / 8;
+	// to blocks:
+	bitmap_blocks = (bitmap_blocks + (blockSize - 1)) / blockSize;
+	// this is how many blocks bitmap will occupy
+	vcb->bitmap_total = bitmap_blocks;
+	return 1;
 }
+// inits bitmap and marks vcb and bitmap blocks as used
+int init_bitmap(){
+	int bytes = vcb->bitmap_total * vcb->block_size;
+	bitmap = malloc(sizeof(BitMap) + bytes * sizeof(int));
+	short used_space = 1 + vcb->bitmap_total;
+	for(int i = 0; i < used_space; i++){
+		bitmap->bitmap[i] = 1; // used
+	}
+	for(int i = used_space + 1; i < bytes; i++){
+		bitmap->bitmap[i] = 0; // unused
+	}
+	return 1;
+}
+// prints bitmap in backwards order
+void test_bitmap(){
+	printf("\n");
+	int bytes = vcb->bitmap_total * vcb->block_size;
+	for(int j = bytes - 1; j >=0; j--){
+		printf("%d\t%d\n", bitmap->bitmap[j], j);
+	}
+	printf("%d\n", bytes);
+}
+
+
 
 void exitFileSystem()
 {
--- fs_structs.h
+++ fs_structs.h
@@ -3,7 +3,7 @@
 #define FS_STRUCTS .H
 #include <stdio.h>
 #include <time.h>
-#define MAX_BLOCKS 1000 // 1000 * 512
+#define MAX_ENTRIES 50
 #define MAGIC_NUMBER 0x4261686100000000
 // Needs to take only 1 block
 typedef struct VCB
@@ -13,33 +13,25 @@ typedef struct VCB
     unsigned int block_size;       // Will be 512
     unsigned int free_blocks;      // Number of free blocks
     unsigned int number_of_blocks; // Total number of blocks in VCB
-    unsigned int bitmap_start;     // Starting pos of bitmap within VCB
-    unsigned int bitmap_end;       // Ending pos of bitmap within VCB
+    unsigned int bitmap_total;    //total number of blocks ocupied by bitmap
 } VCB;
-
-// 1000
-// VCB = start, end
-// Root = 20
-// 0 = VCB
-// 1 = BitMap
-// 2... 22
-// 3 = Home -> 23 and 43
-// 4 = pics -> 44 and 64 ....
-// Bitmap = 0  o 64, = 1
-// Could take multiple blocks
 typedef struct BitMap
-{
-    unsigned int blocks;
+{   size_t size;
     unsigned int bitmap[]; // To keep track of free spaces
 } BitMap;
 
 typedef struct DirectoryEntry
-{                                    // home -> 20
-                                     // [1,2,3], 4, 5 , [6,7], 8 .....] 3
-    char file_name[128];             // File Name
-    unsigned int data_locations[20]; // Array containing locations of file
-    unsigned int type;               // Either File or Directory Entry
-    unsigned int file_size;          // File Size in Bytes
-    time_t creation_date;            // When was it Created
+{                              
+    char file_name[128];          // File Name
+    unsigned int data_locations[MAX_ENTRIES]; // Array containing locations of file
+    unsigned int type;       // Either File or Directory Entry
+    unsigned int file_size;       // File Size in Bytes
+    time_t creation_date;         // When was it Created
+    time_t last_access;           // when it was last accessed
+    time_t last_mod;              // when it was last modified
 } DirectoryEntry;
+
+int init_vcb(int, int);
+int init_bitmap();
+void test_bitmap();
 #endif
