9385c567d7e1a96fc7a9ea355308203b3afdeaf3
Student <mdahbour@sfsu.edu>
2023-04-06 01:26:50 -0700

    Milestone 1 99% done -Diego+Moe

--- fsInit.c
+++ fsInit.c
@@ -35,17 +35,20 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 	printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
 	vcb = malloc(blockSize);
-
 	LBAread(vcb, 1, 0);
 	if (vcb->magic_n == MAGIC_NUMBER)
 	{
 		free(vcb);
 		return 1;
 	}
+	//int result = startPartitionSystem("test", &numberOfBlocks, &blockSize);
+
 	init_vcb(blockSize, numberOfBlocks);
 	init_bitmap();
 	init_root(blockSize);
 	test_bitmap();
+	//printf("Result: %d\n", result);
+	//closePartitionSystem();
 
 	// DirEntry
 	return 0;
@@ -64,36 +67,65 @@ int init_vcb(uint64_t blockSize, uint64_t numberOfBlocks)
 	bitmap_blocks = (bitmap_blocks + (blockSize - 1)) / blockSize;
 	// this is how many blocks bitmap will occupy
 	vcb->bitmap_total = bitmap_blocks;
+	LBAwrite(vcb, 1, 0);
 	return 1;
 }
 
 int init_root(uint64_t blockSize)
 {
-	
-	int size_to_be_malloced= MAX_ENTRIES*sizeof(DirectoryEntry);
-	int num_of_blocks= (size_to_be_malloced+(blockSize-1))/blockSize;
-	dir_entry = malloc(size_to_be_malloced);
-	printf("SIZE NEEDED: %d\nnum_Of_Blocks: %d\n Size of struct: %d\n",
-	size_to_be_malloced,num_of_blocks,sizeof(DirectoryEntry));
+	DirectoryEntry dir_entries[MAX_ENTRIES];
+	int size = MAX_ENTRIES * sizeof(DirectoryEntry);
+	int num_of_blocks = (size + (blockSize - 1)) / blockSize;
+	//dir_entry = malloc(size_to_be_malloced);
+	printf("SIZE NEEDED: %d\nnum_Of_Blocks: %d\n Size of struct: %ld\n",
+		   size, num_of_blocks, sizeof(DirectoryEntry));
 
+	// if (dir_entry == NULL)
+	// {
+	// 	perror("Failed to allocate memory for the root directory");
+	// 	return -1;
+	// }
+	// each has locations for their data
+	printf("%d\n", MAX_ENTRIES);
+	dir_entries[0].file_name[0] = '.';
+	//..
+	dir_entries[1].file_name[0] = '.';
+	dir_entries[1].file_name[1] = '.';
 
-	if (dir_entry == NULL)
-	{
-		perror("Failed to allocate memory for the root directory");
-		return -1;
-	}
+	dir_entries[0].type = DIR;
+	dir_entries[0].file_size = size;
+	dir_entries[0].creation_date = time(NULL);
+	dir_entries[0].last_access = time(NULL);
+	dir_entries[0].last_mod = time(NULL);
+
+	dir_entries[1].type = DIR;
+	dir_entries[1].file_size = size;
+	dir_entries[1].creation_date = time(NULL);
+	dir_entries[1].last_access = time(NULL);
+	dir_entries[1].last_mod = time(NULL);
+
+	unsigned int root_index = get_next_free();
+	vcb->root = root_index;
 	for (int i = 0; i < MAX_ENTRIES; i++)
-	{	
-		dir_entry[i].file_name[0]=' ';
-		for(int j=0; j<MAX_ENTRIES;j++){
-			dir_entry[i].data_locations[j]=0;
-		}
-		dir_entry[i].type=0;
-		dir_entry[i].file_size=0;
-		dir_entry[i].creation_date=time(NULL);
-		dir_entry[i].last_access=time(NULL);
-		dir_entry[i].last_mod=time(NULL);
+	{
+		dir_entries[0].data_locations[i] = root_index;
+		dir_entries[1].data_locations[i] = root_index;
+		bitmap->bitmap[root_index] = USED;
+		root_index++;
 	}
+
+	for (int i = 0; i < MAX_ENTRIES; i++)
+	{
+		dir_entries[i].file_name[0] = ' ';
+		dir_entries[i].type = -1;
+		dir_entries[i].file_size = 0;
+	}
+	short used = 1 + vcb->bitmap_total;
+	vcb->bitmap_total += num_of_blocks;
+	vcb->free_blocks -= num_of_blocks;
+
+	LBAwrite(&dir_entries, num_of_blocks, used);
+	return 1;
 }
 
 // inits bitmap and marks vcb and bitmap blocks as used
@@ -104,12 +136,13 @@ int init_bitmap()
 	short used_space = 1 + vcb->bitmap_total;
 	for (int i = 0; i < used_space; i++)
 	{
-		bitmap->bitmap[i] = 1; // used
+		bitmap->bitmap[i] = USED;
 	}
 	for (int i = used_space + 1; i < bytes; i++)
 	{
-		bitmap->bitmap[i] = 0; // unused
+		bitmap->bitmap[i] = UNUSED;
 	}
+	LBAwrite(bitmap, vcb->bitmap_total, 1);
 	return 1;
 }
 // prints bitmap in backwards order
@@ -122,8 +155,19 @@ void test_bitmap()
 	// }
 	printf("%d\n", bytes);
 }
-int return_index_of_free_blocks(int number_of_requested_blocks){
 
+// returns index in blocks
+int get_next_free()
+{
+	int bytes = vcb->bitmap_total * vcb->block_size;
+	for (int i = 0; i < bytes; i++)
+	{
+		if (bitmap->bitmap[i] == UNUSED)
+		{
+			return i;
+		}
+	}
+	return -1;
 }
 void exitFileSystem()
 {
--- fs_structs.h
+++ fs_structs.h
@@ -1,9 +1,12 @@
-
 #ifndef FS_STRUCTS
 #define FS_STRUCTS .H
 #include <stdio.h>
 #include <time.h>
 #define MAX_ENTRIES 50
+#define UNUSED 0
+#define USED 1
+#define FILE 1
+#define DIR 0
 #define MAGIC_NUMBER 0x4261686100000000
 // Needs to take only 1 block
 typedef struct VCB
@@ -13,10 +16,11 @@ typedef struct VCB
     unsigned int block_size;       // Will be 512
     unsigned int free_blocks;      // Number of free blocks
     unsigned int number_of_blocks; // Total number of blocks in VCB
-    unsigned int bitmap_total;    //total number of blocks ocupied by bitmap
+    unsigned int bitmap_total;     //total number of blocks ocupied by bitmap
 } VCB;
 typedef struct BitMap
-{   size_t size;
+{
+    size_t size;
     unsigned int bitmap[]; // To keep track of free spaces
 } BitMap;
 
@@ -24,38 +28,30 @@ typedef struct BitMap
 Lets say we have 50 max entries, which is 36 blocks, and then that means
 let's say you want entry number 5, how would you go about finding that?
 sizeof(DirectoryEntry), which gives us 360 bytes, so the 5th entry would be
-360*5 which would give us the starting byte. If we want the starting block
-we divide (360*5)/512=#of block we're currently on.
+3605 which would give us the starting byte. If we want the starting block
+we divide (3605)/512=#of block we're currently on.
 */
 //Padding could be useful
-//Add more chars to file and parent name to avoid padding? 
-
-
+//Add more chars to file and parent name to avoid padding?
 
 //. -->parent ' '
 //. --> data_locations[0].name '..'?
 
-
-
 typedef struct DirectoryEntry
-{   
-    char parent_name[128];         //Parent Name                           
-    char file_name[128];          // File Name
+{                                             // padding + 50
+    char parent_name[128];                    //Parent Name
+    char file_name[128];                      // File Name
     unsigned int data_locations[MAX_ENTRIES]; // Array containing locations of file
-    unsigned int type;       // Either File or Directory Entry
-    unsigned int file_size;       // File Size in Bytes
-    time_t creation_date;         // When was it Created
-    time_t last_access;           // when it was last accessed
-    time_t last_mod;              // when it was last modified
+    unsigned int type;                        // Either File or Directory Entry
+    unsigned int file_size;                   // File Size in Bytes
+    time_t creation_date;                     // When was it Created
+    time_t last_access;                       // when it was last accessed
+    time_t last_mod;                          // when it was last modified
 } DirectoryEntry;
-
-typedef struct DirectoryEntries{
-    DirectoryEntry dir_entry[MAX_ENTRIES];
-    
-}DirectoryEntries;
 
 int init_vcb(uint64_t, uint64_t);
 int init_bitmap();
 int init_root(uint64_t);
+int get_next_free();
 void test_bitmap();
 #endif
